e42cbc7f77290da2f349ec8f44c0d76d34bc5073
Fabien Bellanger <fabien.bellanger@apitic.com>
2021-05-06 13:00:20 +0200

    Goo Space age finished

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -1,3 +1,44 @@
 package space
 
+import (
+	"errors"
+)
+
+// Planet represents the planet name.
 type Planet string
+
+const SECS_IN_EARTH_YEAR = 31557600.0 // 3600 * 24 * 365.25
+
+func (p *Planet) orbital() (y float64, err error) {
+	switch *p {
+	case "Earth":
+		y = 1.0
+	case "Mercury":
+		y = 0.2408467
+	case "Venus":
+		y = 0.61519726
+	case "Mars":
+		y = 1.8808158
+	case "Jupiter":
+		y = 11.862615
+	case "Saturn":
+		y = 29.447498
+	case "Uranus":
+		y = 84.016846
+	case "Neptune":
+		y = 164.79132
+	default:
+		err = errors.New("invalid planet")
+	}
+
+	return
+}
+
+// Age calculates age for planet form sec.
+func Age(sec float64, planet Planet) (age float64) {
+	orbibal, err := planet.orbital()
+	if err != nil {
+		return -1
+	}
+	return sec / SECS_IN_EARTH_YEAR / orbibal
+}
