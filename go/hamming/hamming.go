629bc4d8d6ce2c20207a95d7979a700d2ce4ff53
Fabien Bellanger <fabien.bellanger@apitic.com>
2021-05-06 13:24:57 +0200

    Go hamming finished

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,4 +1,22 @@
 package hamming
 
-func Distance(a, b string) (int, error) {
+import "errors"
+
+// Distance returns Hamming distance if strings are corrects, else it returns an error.
+func Distance(a, b string) (d int, err error) {
+	if len(a) != len(b) {
+		err = errors.New("strings with different length")
+		return
+	}
+
+	if a == "" {
+		return
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			d++
+		}
+	}
+	return
 }
