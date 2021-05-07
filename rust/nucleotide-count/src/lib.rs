7d0fad2eaa4978d9e6b523e8d067cafa222eccc4
Fabien Bellanger <valentil@gmail.com>
2021-05-07 22:13:23 +0200

    Debut Rust Nucleotide

---
+++ rust/nucleotide-count/src/lib.rs
@@ -0,0 +1,15 @@
+use std::collections::HashMap;
+
+pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
+    match nucleotide {
+        'A' | 'C' | 'G' | 'T' => Ok(dna.chars().filter(|c| *c == nucleotide).count()),
+        _ => Err(nucleotide),
+    }
+}
+
+pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
+    unimplemented!(
+        "How much of every nucleotide type is contained inside DNA string '{}'?",
+        dna
+    );
+}
