24fc98a39db27f2d12b766294ec0498bc59bedf4
Fabien Bellanger <fabien.bellanger@apitic.com>
2021-05-12 19:17:24 +0200

    Rust Triangle finished

--- rust/triangle/src/lib.rs
+++ rust/triangle/src/lib.rs
@@ -1,34 +1,51 @@
-use std::fmt::Debug;
-use std::cmp::PartialOrd;
+use std::{
+    collections::HashSet,
+    fmt::{Debug, Display},
+    ops::Add,
+};
 
 #[derive(Debug)]
-pub struct Triangle<T> 
-where T: Debug + PartialOrd + Default {
+pub struct Triangle<T> {
     sides: [T; 3],
 }
 
 impl<T> Triangle<T>
-where T: Debug + PartialOrd + Default {
+where
+    T: Debug + Display + PartialOrd + PartialEq + Default + Copy + Add<Output = T>,
+{
     pub fn build(sides: [T; 3]) -> Option<Triangle<T>> {
-        dbg!(T::default());
-        // if sides.iter().any(|s|  (*s).partial_cmp(&T::default()).is_some()) {
-        //     return None;
-        // }
-        // Some(Self{
-        //     sides
-        // })
-        None
+        if sides.iter().any(|s| s <= &T::default()) {
+            return None;
+        }
+
+        // Valid triangle ?
+        if (sides[0] > sides[1] + sides[2])
+            || (sides[1] > sides[0] + sides[2])
+            || (sides[2] > sides[0] + sides[1])
+        {
+            return None;
+        }
+
+        Some(Self { sides })
     }
 
     pub fn is_equilateral(&self) -> bool {
-        unimplemented!("Determine if the Triangle is equilateral.");
+        self.sides_number() == 1
     }
 
     pub fn is_scalene(&self) -> bool {
-        unimplemented!("Determine if the Triangle is scalene.");
+        self.sides_number() == 3
     }
 
     pub fn is_isosceles(&self) -> bool {
-        unimplemented!("Determine if the Triangle is isosceles.");
+        self.sides_number() == 2
+    }
+
+    fn sides_number(&self) -> usize {
+        self.sides
+            .iter()
+            .map(|side| (*side).to_string())
+            .collect::<HashSet<String>>()
+            .len()
     }
 }
