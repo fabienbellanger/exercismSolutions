f1dccd17fe769aedfbb6568c441a91e98d13b325
Fabien Bellanger <fabien.bellanger@apitic.com>
2021-10-04 08:52:36 +0200

    Begin In Go

--- go/scale-generator/scale_generator.go
+++ go/scale-generator/scale_generator.go
@@ -1,1 +1,19 @@
 package scale
+
+const (
+	mininor = iota + 1
+)
+
+func Scale(tonic, interval string) []string {
+	result := make([]string, 0)
+	interval, err := testInterval(interval)
+	if err != nil {
+		panic("invalid interval")
+	}
+
+	return result
+}
+
+func testInterval(i string) (interval string, err error) {
+	return
+}
