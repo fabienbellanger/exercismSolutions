3bfc91024283b35fffa8b90d4dd3592eb6543a8c
Fabien Bellanger <fabien.bellanger@apitic.com>
2022-06-16 15:54:32 +0200

    Finished Rust Magazine cutout

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -1,9 +1,20 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    let mut words = HashMap::new();
+
+    for word in magazine {
+        let entry = words.entry(*word).or_insert(0);
+        *entry += 1;
+    }
+
+    for word in note {
+        let w = words.entry(*word).or_insert(0);
+        match w {
+            0 => return false,
+            _ => *w -= 1,
+        }
+    }
+
+    true
 }
