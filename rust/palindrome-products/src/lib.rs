59eee33062783758b4497af28485eb304ca1f0d9
Fabien <fabienbellanger@users.noreply.github.com>
2022-07-06 23:31:36 +0200

    Continue Palindrome products

--- rust/palindrome-products/src/lib.rs
+++ rust/palindrome-products/src/lib.rs
@@ -8,15 +8,27 @@ pub struct Palindrome(u64);
 impl Palindrome {
     /// Create a `Palindrome` only if `value` is in fact a palindrome when represented in base ten. Otherwise, `None`.
     pub fn new(value: u64) -> Option<Palindrome> {
-        unimplemented!(
-            "if the value {} is a palindrome return Some, otherwise return None",
-            value
-        );
+        let mut to_bytes: Vec<u32> = vec![];
+        
+        for d in value.to_string().chars() {
+            match d.to_digit(10) {
+                Some(d) => to_bytes.push(d),
+                None => return None,
+            }
+        }
+
+        let mut to_bytes_reversed = to_bytes.clone();
+        to_bytes_reversed.reverse();
+        
+        if to_bytes == to_bytes_reversed {
+            return Some(Self(value));
+        }
+        None
     }
 
     /// Get the value of this palindrome.
     pub fn into_inner(self) -> u64 {
-        unimplemented!("return inner value of a Palindrome");
+        self.0
     }
 }
 
