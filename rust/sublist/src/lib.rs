421e645f191d4d4a115861b0e4661e6756bff6f0
Fabien Bellanger <fabien.bellanger@apitic.com>
2022-07-08 18:35:39 +0200

    Finished Rust Sublist

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 #[derive(Debug, PartialEq)]
 pub enum Comparison {
     // A = [1, 2, 3], B = [1, 2, 3], A is equal to B
@@ -8,11 +10,44 @@ pub enum Comparison {
 
     // A = [1, 2, 3, 4, 5], B = [2, 3, 4], A is a superlist of B
     Superlist,
-    
+
     // A = [1, 2, 4], B = [1, 2, 3, 4, 5], A and B are unequal
     Unequal,
 }
 
-pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+pub fn sublist<T: PartialEq + Debug>(first_list: &[T], second_list: &[T]) -> Comparison {
+    // Equal
+    if first_list.eq(second_list) {
+        return Comparison::Equal;
+    }
+
+    let first_list_len = first_list.len();
+    let second_list_len = second_list.len();
+
+    // Sublist
+    if first_list.is_empty() {
+        return Comparison::Sublist;
+    }
+    if first_list_len < second_list_len {
+        let is_sublist = second_list.windows(first_list_len).any(|l| l == first_list);
+        if is_sublist {
+            return Comparison::Sublist;
+        }
+    }
+
+    // Superlist
+    if second_list.is_empty() {
+        return Comparison::Superlist;
+    }
+    if second_list_len < first_list_len {
+        let is_superlist = first_list
+            .windows(second_list_len)
+            .any(|l| l == second_list);
+        if is_superlist {
+            return Comparison::Superlist;
+        }
+    }
+
+    // Unequal
+    Comparison::Unequal
 }
