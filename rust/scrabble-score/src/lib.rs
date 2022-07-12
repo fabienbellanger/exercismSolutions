e958859b00a59c17803e40f8d29e7998865e64e4
Fabien Bellanger <fabien.bellanger@apitic.com>
2022-07-12 14:00:30 +0200

    Finished Rust Scrabble score

---
+++ rust/scrabble-score/src/lib.rs
@@ -0,0 +1,20 @@
+/// Compute the Scrabble score for a word.
+pub fn score(word: &str) -> u64 {
+    word.chars()
+        .filter(|c| c.is_ascii_alphabetic())
+        .map(|c| match c.is_ascii_uppercase() {
+            true => c.to_ascii_lowercase(),
+            false => c,
+        })
+        .map(|c| match c {
+            'a' | 'e' | 'i' | 'o' | 'u' | 'l' | 'n' | 'r' | 's' | 't' => 1,
+            'd' | 'g' => 2,
+            'b' | 'c' | 'm' | 'p' => 3,
+            'f' | 'h' | 'v' | 'w' | 'y' => 4,
+            'k' => 5,
+            'j' | 'x' => 8,
+            'q' | 'z' => 10,
+            _ => 0
+        })
+        .sum()
+}
