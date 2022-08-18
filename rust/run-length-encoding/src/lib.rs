a921cf2bc86e1c2815da82cb89fdeabc2409387a
Fabien Bellanger <fabien.bellanger@apitic.com>
2022-08-18 14:04:10 +0200

    Finished Rust Run length encoding

--- rust/run-length-encoding/src/lib.rs
+++ rust/run-length-encoding/src/lib.rs
@@ -1,40 +1,29 @@
-use std::fmt::Write;
-
 pub fn encode(source: &str) -> String {
     let mut s = String::new();
-
-    if source.is_empty() {
-        return s;
-    }
-
     let mut current_char = None;
     let mut counter = 0;
+    let source_length = source.len();
+
     for (idx, c) in source.chars().enumerate() {
         if current_char.is_none() {
             current_char = Some(c);
             counter += 1;
         } else if Some(c) == current_char {
             counter += 1;
-
-            if idx == source.len() - 1 {
-                if counter == 1 {
-                    s.push(current_char.unwrap());
-                } else {
-                    write!(&mut s, "{}{}", counter, current_char.unwrap()).unwrap();
-                }
-            }
         } else {
-            if counter == 1 {
-                s.push(current_char.unwrap());
-            } else {
-                write!(&mut s, "{}{}", counter, current_char.unwrap()).unwrap();
+            if counter > 1 {
+                s.push_str(&counter.to_string());
             }
+            s.push(current_char.unwrap_or_default());
             current_char = Some(c);
             counter = 1;
+        }
 
-            if idx == source.len() - 1 {
-                s.push(current_char.unwrap());
+        if idx == source_length - 1 {
+            if counter > 1 {
+                s.push_str(&counter.to_string());
             }
+            s.push(c);
         }
     }
 
@@ -42,5 +31,20 @@ pub fn encode(source: &str) -> String {
 }
 
 pub fn decode(source: &str) -> String {
-    unimplemented!("Return the run-length decoding of {}.", source);
+    let mut s = String::new();
+    let mut number = String::new();
+
+    for c in source.chars() {
+        if c.is_ascii_digit() {
+            number.push(c);
+        } else {
+            let n = number.parse::<usize>().unwrap_or(1);
+            let part = (0..n).map(|_| c).collect::<String>();
+            s.push_str(&part);
+
+            number = "".to_owned();
+        }
+    }
+
+    s
 }
