0020664492f2b80d4a2c30b18a3d748a31e6b3bb
Fabien Bellanger <fabien.bellanger@apitic.com>
2022-08-18 15:39:38 +0200

    Better solution with peekable iterator

--- rust/run-length-encoding/src/lib.rs
+++ rust/run-length-encoding/src/lib.rs
@@ -1,4 +1,44 @@
 pub fn encode(source: &str) -> String {
+    let mut encoded_str = String::new();
+    let mut chars = source.chars().peekable();
+    let mut counter = 1;
+
+    while let Some(current_char) = chars.next() {
+        if Some(&current_char) == chars.peek() {
+            counter += 1;
+        } else {
+            if counter > 1 {
+                encoded_str.push_str(&counter.to_string());
+            }
+            encoded_str.push(current_char);
+
+            counter = 1;
+        }
+    }
+    
+    encoded_str
+}
+
+pub fn decode(source: &str) -> String {
+    let mut s = String::new();
+    let mut number = String::new();
+
+    for c in source.chars() {
+        if c.is_ascii_digit() {
+            number.push(c);
+        } else {
+            let n = number.parse::<usize>().unwrap_or(1);
+            let part = (0..n).map(|_| c).collect::<String>();
+            s.push_str(&part);
+
+            number = "".to_owned();
+        }
+    }
+
+    s
+}
+
+fn _encode_old(source: &str) -> String {
     let mut s = String::new();
     let mut current_char = None;
     let mut counter = 0;
@@ -24,25 +64,6 @@ pub fn encode(source: &str) -> String {
                 s.push_str(&counter.to_string());
             }
             s.push(c);
-        }
-    }
-
-    s
-}
-
-pub fn decode(source: &str) -> String {
-    let mut s = String::new();
-    let mut number = String::new();
-
-    for c in source.chars() {
-        if c.is_ascii_digit() {
-            number.push(c);
-        } else {
-            let n = number.parse::<usize>().unwrap_or(1);
-            let part = (0..n).map(|_| c).collect::<String>();
-            s.push_str(&part);
-
-            number = "".to_owned();
         }
     }
 
