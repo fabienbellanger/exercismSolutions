cf3504f5432343c12632413d01afe2ba52394740
Fabien Bellanger <fabien.bellanger@apitic.com>
2022-08-24 18:11:05 +0200

    Finished Rust Macros

--- rust/macros/src/lib.rs
+++ rust/macros/src/lib.rs
@@ -1,6 +1,15 @@
-#[macro_export]
+#[macro_export(local_inner_macros)]
 macro_rules! hashmap {
-    () => {
-        unimplemented!()
+    ( $( $key:expr => $value:expr, )+ ) => { hashmap!($($key => $value),+) };
+    ( $( $key:expr => $value:expr ),* ) => {
+        {
+            let mut tmp_hashmap = ::std::collections::HashMap::new();
+
+            $(
+                tmp_hashmap.insert({$key}, {$value});
+            )*
+
+            tmp_hashmap
+        }
     };
 }
