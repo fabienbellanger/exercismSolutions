418e6a0df66b660e0594bdb5aaed31965b459ecd
Fabien Bellanger <fabien.bellanger@apitic.com>
2023-10-24 13:45:24 +0200

    Finished Zig Hello World

---
+++ zig/difference-of-squares/difference_of_squares.zig
@@ -0,0 +1,13 @@
+pub fn squareOfSum(number: usize) usize {
+    _ = number;
+    @compileError("compute the sum of i from 0 to n then square it");
+}
+
+pub fn sumOfSquares(number: usize) usize {
+    _ = number;
+    @compileError("compute the sum of i^2 from 0 to n");
+}
+
+pub fn differenceOfSquares(number: usize) usize {
+    return squareOfSum(number) - sumOfSquares(number);
+}
