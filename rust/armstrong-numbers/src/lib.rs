f4f233f4645ba6bbbc540996ba9dd39449abc5d4
Fabien Bellanger <fabien.bellanger@apitic.com>
2023-11-23 14:48:13 +0100

    Fix updated exercice

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,5 @@
+use std::convert::TryFrom;
+
 pub fn is_armstrong_number(num: u32) -> bool {
     let numbers: Vec<u32> = num
         .to_string()
@@ -5,6 +7,13 @@ pub fn is_armstrong_number(num: u32) -> bool {
         .map(|i| i.to_digit(10).unwrap())
         .collect();
     let numbers_length = numbers.len();
-    let result: u32 = numbers.iter().map(|i| i.pow(numbers_length as u32)).sum();
-    result == num
+    let result: u64 = numbers
+        .iter()
+        .map(|i| i.pow(numbers_length as u32) as u64)
+        .sum();
+
+    match u32::try_from(result) {
+        Ok(result) => result == num,
+        _ => false,
+    }
 }
