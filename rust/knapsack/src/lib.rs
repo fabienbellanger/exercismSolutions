0fe5d42bcc35490a9e9ea7912120d18efbe08c1b
Fabien Bellanger <fabien.bellanger@apitic.com>
2024-03-22 18:30:23 +0100

    Finished Knapsack Rust

--- rust/knapsack/src/lib.rs
+++ rust/knapsack/src/lib.rs
@@ -5,5 +5,23 @@ pub struct Item {
 }
 
 pub fn maximum_value(max_weight: u32, items: &[Item]) -> u32 {
-    todo!("calculate the maximum value achievable with the given {items:?} and {max_weight}");
+    let n = items.len();
+    let mut t = vec![vec![0; max_weight as usize + 1]; n + 1];
+
+    for c in 0..=max_weight {
+        t[0][c as usize] = 0;
+    }
+
+    for i in 1..=n {
+        for c in 0..=max_weight {
+            if c >= items[i - 1].weight {
+                t[i][c as usize] = t[i - 1][c as usize]
+                    .max(t[i - 1][(c - items[i - 1].weight) as usize] + items[i - 1].value);
+            } else {
+                t[i][c as usize] = t[i - 1][c as usize];
+            }
+        }
+    }
+
+    t[n][max_weight as usize]
 }
