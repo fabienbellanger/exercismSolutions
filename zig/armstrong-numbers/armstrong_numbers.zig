54e6ac51afcfe5a23dea8d4fb746971424fbb936
Fabien Bellanger <fabien.bellanger@apitic.com>
2024-04-03 10:11:48 +0200

    Finished Zig Armstrong numbers

--- zig/armstrong-numbers/armstrong_numbers.zig
+++ zig/armstrong-numbers/armstrong_numbers.zig
@@ -4,9 +4,14 @@ const log = std.debug;
 pub fn isArmstrongNumber(num: u128) bool {
     var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = general_purpose_allocator.allocator();
-    const num_str = std.fmt.allocPrint(allocator, "{d}", .{num}) catch unreachable;
-    const num_length = num_str.len();
-    _ = num_length;
+    const num_str: []u8 = std.fmt.allocPrint(allocator, "{d}", .{num}) catch unreachable;
+    const length = num_str.len;
 
-    return true;
+    var sum: u128 = 0;
+    for (num_str) |c| {
+        const digit = c - '0';
+        sum += std.math.pow(u128, digit, length);
+    }
+
+    return num == sum;
 }
