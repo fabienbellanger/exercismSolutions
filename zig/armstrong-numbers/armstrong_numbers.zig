db010d21660d8090a4e86ab00a3c350819cfbf8c
Fabien BELLANGER <valentil@gmail.com>
2026-04-25 22:51:49 +0200

    Fix Zig exercices whith Zig 0.16

--- zig/armstrong-numbers/armstrong_numbers.zig
+++ zig/armstrong-numbers/armstrong_numbers.zig
@@ -1,15 +1,14 @@
 const std = @import("std");
 
 pub fn isArmstrongNumber(num: u128) bool {
-    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = general_purpose_allocator.allocator();
-    const num_str: []u8 = std.fmt.allocPrint(allocator, "{d}", .{num}) catch unreachable;
-    const length = num_str.len;
+    var length: u128 = 1;
+    var n = num;
+    while (n >= 10) : (n /= 10) length += 1;
 
     var sum: u128 = 0;
-    for (num_str) |c| {
-        const digit = c - '0';
-        sum += std.math.powi(u128, digit, length) catch unreachable;
+    n = num;
+    while (n != 0) : (n /= 10) {
+        sum += std.math.powi(u128, n % 10, length) catch unreachable;
     }
 
     return num == sum;
--- zig/pangram/pangram.zig
+++ zig/pangram/pangram.zig
@@ -2,8 +2,8 @@ const std = @import("std");
 const ascii = std.ascii;
 
 pub fn isPangram(str: []const u8) bool {
-    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = general_purpose_allocator.allocator();
+    var debug_allocator = std.heap.DebugAllocator(.{}){};
+    const allocator = debug_allocator.allocator();
 
     var set = std.AutoHashMap(u8, void).init(allocator);
     defer set.deinit();
