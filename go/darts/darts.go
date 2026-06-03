b402d219fbaf47934a2e7c3a7b28efd82b76a240
Fabien BELLANGER <valentil@gmail.com>
2026-06-03 21:33:38 +0200

    Go Darts OK

--- go/darts/darts.go
+++ go/darts/darts.go
@@ -1,5 +1,15 @@
 package darts
 
 func Score(x, y float64) int {
-	panic("Please implement the Score function")
+	z := x*x + y*y
+
+	if z <= 1 {
+		return 10
+	} else if z <= 25 {
+		return 5
+	} else if z <= 100 {
+		return 1
+	} else {
+		return 0
+	}
 }
